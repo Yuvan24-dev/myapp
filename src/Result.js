2fa44759142bce269e4cff2994ca77d8b06723cc
kishore 01 <vkkishore886@gmail.com>
2024-10-28 11:59:27 +0530

    todays commit
    checking setup

---
+++ src/Result.js
@@ -0,0 +1,11 @@
+function Result(){
+    // <div id="result">Result</div>
+    // var result=document.getElementById("result");
+    // return result;
+    return (
+        <div>
+            <h1>Hello</h1>
+        </div>
+    );
+}
+export default Result ;
