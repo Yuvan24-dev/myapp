b2a9329e89ca043567daadbf44d59d1bcc2ec44d
kishore 01 <vkkishore886@gmail.com>
2024-10-30 11:47:47 +0530

    "Nav bar created"

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Herojs from './Hero.js';
 function App() {
   return (
-    <div className='Full_body'>
+    <div className='px-sm-4 px-xl-5 py-sm-4 py-xl-4 '>
     <Herojs />    
     </div>
   );
