21b3d989d83438cd1040567f10b0ce98790c52e7
Yuvan24-dev <yuvanshankarrajakalimuthu@gmail.com>
2024-11-03 20:19:14 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
 import Herojs from './Hero.js';
+import SliderSection from './Slider.js'
 function App() {
   return (
-    <div className='px-sm-4 px-xl-5 py-sm-4 py-xl-4 '>
+   <div className='px-sm-4 px-xl-5 py-sm-4 py-xl-4 '>
+    <section className='hero_area Full_body'>
     <Herojs />    
+    <SliderSection />
+    </section>
     </div>
   );
 }
--- src/Hero.js
+++ src/Hero.js
@@ -10,30 +10,29 @@ import NavDropdown from 'react-bootstrap/NavDropdown';
 import { IoSearch } from "react-icons/io5";
 import { IoPersonSharp } from "react-icons/io5";
 function Herojs() {
-return (
-<div className='hero_area Full_body'>
-<header>
-<container fluid className="">
-    <Navbar expand="lg" className="bg-body-tertiary">
-      <Container fluid>
-        <Navbar.Brand href="#" className='navbar-brand'>Navbar scroll</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll"className='nav-right'>
-          <Nav>
-            <Nav.Link href="#action1"className='px-lg-5'>Home</Nav.Link>
-            <Nav.Link href="#action1"claclearssName='px-lg-5'>Home</Nav.Link>
-            <Nav.Link href="#action1"className='px-lg-5' >Home</Nav.Link>
-            <Nav.Link href="#action1"className='px-lg-5' >Home</Nav.Link>
-            <Nav.Link href="#action1"className='px-lg-3' >Home</Nav.Link>
-            <Nav.Link href="#action1"className='px-lg-2 px-xl-3' ><IoSearch /></Nav.Link>
-            <Nav.Link href="#action1"className='px-lg-2 px-xl-3' ><IoPersonSharp /></Nav.Link>
-          </Nav>         
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-</container>
-</header>
-</div>
-)};
+  return (
+      <header>
+        <container fluid className="">
+          <Navbar expand="lg" className="bg-body-tertiary">
+            <Container fluid>
+              <Navbar.Brand href="#" className='navbar-brand'>Navbar scroll</Navbar.Brand>
+              <Navbar.Toggle aria-controls="navbarScroll" />
+              <Navbar.Collapse id="navbarScroll" className='nav-right'>
+                <Nav>
+                  <Nav.Link href="#action1" className='px-lg-5'>Home</Nav.Link>
+                  <Nav.Link href="#action1" claclearssName='px-lg-5'>Home</Nav.Link>
+                  <Nav.Link href="#action1" className='px-lg-5' >Home</Nav.Link>
+                  <Nav.Link href="#action1" className='px-lg-5' >Home</Nav.Link>
+                  <Nav.Link href="#action1" className='px-lg-3' >Home</Nav.Link>
+                  <Nav.Link href="#action1" className='px-lg-2 px-xl-3' ><IoSearch /></Nav.Link>
+                  <Nav.Link href="#action1" className='px-lg-2 px-xl-3' ><IoPersonSharp /></Nav.Link>
+                </Nav>
+              </Navbar.Collapse>
+            </Container>
+          </Navbar>
+        </container>
+      </header>
+  )
+};
 export default Herojs;
 
