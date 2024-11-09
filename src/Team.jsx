797a7c9bc7663aff48a37685e7d554ffd177ac25
kishore 01 <vkkishore886@gmail.com>
2024-11-09 11:45:14 +0530

    "alomost"

--- src/Team.jsx
+++ src/Team.jsx
@@ -1,30 +1,93 @@
 import React from 'react';
 import { Container, Row, Col, Card } from 'react-bootstrap';
 import './App.css';
-
+import team1 from './images/team-1.jpg';  
+import team2 from './images/team-2.jpg';  
+import team3 from './images/team-3.jpg';  
+import team4 from './images/team-4.jpg'; 
+import { TiSocialFacebook } from "react-icons/ti";
+import { FaTwitter } from "react-icons/fa";
+import { FaLinkedinIn } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa6";
 const teamMembers = [
-    { name: "Joseph Brown", role: "Marketing Head" },
-    { name: "Nancy White", role: "Marketing Head" },
-    // Additional members
+    { 
+        name: "Joseph Brown", 
+        role: "Marketing Head", 
+        image: team1, 
+        social: [
+            <TiSocialFacebook />,
+            <FaTwitter />,
+            <FaLinkedinIn />,
+            <FaInstagram />,
+            <FaYoutube />
+        ] 
+    },
+    { 
+        name: "Nancy White", 
+        role: "Marketing Head", 
+        image: team2, 
+        social: [
+            <TiSocialFacebook />,
+            <FaTwitter />,
+            <FaLinkedinIn />,
+            <FaInstagram />,
+            <FaYoutube />
+        ]
+    },
+    { 
+        name: "John Doe", 
+        role: "Sales Manager", 
+        image: team3, 
+        social: [
+            <TiSocialFacebook />,
+            <FaTwitter />,
+            <FaLinkedinIn />,
+            <FaInstagram />,
+            <FaYoutube />
+        ] 
+    },
+    { 
+        name: "Emily Smith", 
+        role: "CEO", 
+        image: team4, 
+        social: [
+            <TiSocialFacebook />,
+            <FaTwitter />,
+            <FaLinkedinIn />,
+            <FaInstagram />,
+            <FaYoutube />
+        ] 
+    },
 ];
 
 const Team = () => (
-    <Container className="team-section py-5">
-        <h2 className="text-center">Our Team</h2>
+    <div className="team_section pt-1 pb-2">
+    <Container className="py-5">
+        <h2 className="text-center py-3 text-white">Our <span style={{color: '#00bbf0'}}>Team</span></h2>
         <Row>
             {teamMembers.map((member, index) => (
-                <Col lg={3} sm={6} key={index}>
-                    <Card className="border-0 text-center text-white bg-dark">
-                        <Card.Img variant="top" src={`team-${index + 1}.jpg`} alt={member.name} />
+               
+               <Col className='px-2' lg={3} sm={6} key={index}>
+                    <Card className="border-0 text-center text-white  py-5 card_d">
+                        <Card.Img className='teamround ' variant="top" src={member.image} alt={member.name} />
                         <Card.Body>
                             <Card.Title>{member.name}</Card.Title>
                             <Card.Text>{member.role}</Card.Text>
                         </Card.Body>
+                        <div className="d-flex justify-content-center">
+                            {member.social.map((icon, iconIndex) => (
+                                <div key={iconIndex} className="social-icon px-3 ">
+                                    {icon}
+                                </div>
+                            ))}
+                        </div>
                     </Card>
                 </Col>
             ))}
         </Row>
     </Container>
+    </div>
 );
 
 export default Team;
--- src/Whyus.jsx
+++ src/Whyus.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-bootstrap';
 import whyimg  from './images/w1.png'
+import whyimg1  from './images/w2.png'
+import whyimg2  from './images/w3.png'
+import whyimg3  from './images/w4.png'
+
 import './App.css';
 
 const WhyUs = () => (
@@ -13,19 +17,22 @@ const WhyUs = () => (
                 <p>We provide the best management for all crypto-related services.</p>
             </Col>
             <Col className='py-4' md={3}>
-               <div className='img_forbd'><img className='img_why' src={whyimg} alt="Icon" /></div> 
-                <h5>Expert Management</h5>
+               <div className='img_forbd'><img className='img_why' src={whyimg1} alt="Icon" /></div> 
+                <h5>Secure Inverstment</h5>
                 <p>We provide the best management for all crypto-related services.</p>
             </Col>
             <Col className='py-4' md={3}>
-               <div className='img_forbd'><img className='img_why' src={whyimg} alt="Icon" /></div> 
-                <h5>Expert Management</h5>
+               <div className='img_forbd'><img className='img_why' src={whyimg2} alt="Icon" /></div> 
+                <h5>Instant Trading</h5>
                 <p>We provide the best management for all crypto-related services.</p>
             </Col>
             <Col className='py-4' md={3}>
-               <div className='img_forbd'><img className='img_why' src={whyimg} alt="Icon" /></div> 
-                <h5>Expert Management</h5>
-                <p>We provide the best management for all crypto-related services.</p>
+            <div className='img_forbd'><img className='img_why' src={whyimg3} alt="Icon" /></div> 
+               <h5>Happy customers</h5>
+                <p>
+              Incidunt odit rerum tenetur alias architecto asperiores omnis cumque doloribus aperiam numquam! Eligendi corrupti, molestias laborum dolores quod nisi vitae voluptate ipsa? In tempore voluptate ducimus officia id, aspernatur nihil.
+              Tempore laborum nesciunt ut veniam, nemo officia ullam repudiandae repellat veritatis unde reiciendis possimus animi autem natus
+            </p>
             </Col>
         </Row>
     </Container>
